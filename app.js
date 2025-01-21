59e55c5b73f0eec5de6aa7d144cd34531ea04ed7
enriquemoralesvite <1412513@utp.edu.pe>
2025-01-21 16:45:23 -0500

    create the game of secret friend

---
+++ app.js
@@ -0,0 +1,27 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//boton adicionar amigos
+// sortear amigos
+
+let amigos =[];
+
+function agregarAmigo() {
+    
+    let nombre = document.getElementById("amigo").value;    
+    amigos.push(nombre); 
+    document.getElementById("amigo").value = ""; //limpiar el input
+}  
+function sortearAmigo() {
+    
+    
+    if (amigos.length == 0) {
+        document.getElementById("resultado").innerHTML = "No hay amigos para sortear";
+    }else{
+        let numero = Math.floor(Math.random() * amigos.length);
+        document.getElementById("resultado").innerHTML = amigos[numero]; //innerHTML es para mostrar en el html
+    }
+}
+
+ 
+
+
+  
